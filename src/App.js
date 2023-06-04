8ed7011a0b70acdd8674ddc4a671af663443af1a
philip <philipbwalya2000@gmail.com>
2023-06-04 22:50:29 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import MovieCard from "./MovieCard";
 
 
 const API_URL = 'http://www.omdbapi.com?apikey=6e910f17'
-const movie1 = {
-  "Title": "The Amazing Spiderman 2 Webb Cut",
-  "Year": "2021",
-  "imdbID": "tt18351128",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
-}
+// const movie1 = {
+//   "Title": "The Amazing Spiderman 2 Webb Cut",
+//   "Year": "2021",
+//   "imdbID": "tt18351128",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
+// }
 
 const App = () => {
   const [movies, setMovies] = useState([]);
